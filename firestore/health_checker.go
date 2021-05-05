e4dfc3eaecf1ccac1ea9ee77034758ceaa1d676a
thanhphu <htp1912@gmail.com>
2021-05-05 19:14:41 +0700

    add config credentials

--- firestore/health_checker.go
+++ firestore/health_checker.go
@@ -4,20 +4,39 @@ import (
 	"cloud.google.com/go/firestore"
 	"context"
 	"google.golang.org/api/option"
+	"google.golang.org/api/transport"
 )
 
 type HealthChecker struct {
-	name      string
-	projectId string
-	opts      []option.ClientOption
+	name        string
+	projectId   string
+	opts        []option.ClientOption
+	credentials []byte
 }
 
 func NewFirestoreHealthChecker(name string, projectId string, opts ...option.ClientOption) *HealthChecker {
 	return &HealthChecker{projectId: projectId, name: name, opts: opts}
 }
 
-func NewHealthChecker(projectId string, opts ...option.ClientOption) *HealthChecker {
+func NewHealthCheckerWithProjectId(projectId string, opts ...option.ClientOption) *HealthChecker {
 	return NewFirestoreHealthChecker("firestore", projectId, opts...)
+}
+func NewHealthChecker(ctx context.Context, credentials []byte, options ...string) *HealthChecker {
+	var name string
+	if len(options) > 0 && len(options[0]) > 0 {
+		name = options[0]
+	} else {
+		name = "firestore"
+	}
+	opts := option.WithCredentialsJSON(credentials)
+	creds, er2 := transport.Creds(ctx, opts)
+	if er2 != nil {
+		panic("Credentials Error: " + er2.Error())
+	}
+	if creds == nil {
+		panic("Error: creds is nil")
+	}
+	return NewFirestoreHealthChecker(name, creds.ProjectID, opts)
 }
 func (s HealthChecker) Name() string {
 	return s.name
