660bef201af8c49bf33fc1119d8dd0f353c23c05
Duc Nguyen <minhduc140583@gmail.com>
2021-05-16 21:47:47 +0700

    Fix bug amq, rabbitmq, sqs

--- rabbitmq/health_checker.go
+++ rabbitmq/health_checker.go
@@ -2,21 +2,23 @@ package rabbitmq
 
 import (
 	"context"
-	"github.com/streadway/amqp"
+	"time"
 )
 
-func NewChannel(url string) (*amqp.Channel, error) {
-	conn, err := amqp.Dial(url)
-	if err != nil {
-		return nil, err
-	}
-
-	return conn.Channel()
+type HealthChecker struct {
+	url     string
+	name    string
+	timeout time.Duration
 }
 
-type HealthChecker struct {
-	url  string
-	name string
+func NewHttpHealthChecker(name, url string, timeouts ...time.Duration) *HealthChecker {
+	var timeout time.Duration
+	if len(timeouts) >= 1 {
+		timeout = timeouts[0]
+	} else {
+		timeout = 4 * time.Second
+	}
+	return &HealthChecker{name: name, url: url, timeout: timeout}
 }
 
 func NewHealthChecker(url string, options ...string) *HealthChecker {
@@ -26,7 +28,7 @@ func NewHealthChecker(url string, options ...string) *HealthChecker {
 	} else {
 		name = "rabbitmq"
 	}
-	return &HealthChecker{url, name}
+	return NewHttpHealthChecker(name, url, 4*time.Second)
 }
 
 func (s *HealthChecker) Name() string {
@@ -35,7 +37,7 @@ func (s *HealthChecker) Name() string {
 
 func (s *HealthChecker) Check(ctx context.Context) (map[string]interface{}, error) {
 	res := make(map[string]interface{})
-	channel, er1 := NewChannel(s.url)
+	channel, er1 := NewChannelWithTimeOut(s.url, s.timeout)
 	if er1 != nil {
 		return res, er1
 	}
--- sqs/health_checker.go
+++ sqs/health_checker.go
@@ -3,12 +3,14 @@ package sqs
 import (
 	"context"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"time"
 )
 
 type HealthChecker struct {
 	Client    *sqs.SQS
 	QueueName *string
 	Service   string
+	Timeout   time.Duration
 }
 
 func NewHealthChecker(client *sqs.SQS, queueName string, options ...string) *HealthChecker {
@@ -20,8 +22,14 @@ func NewHealthChecker(client *sqs.SQS, queueName string, options ...string) *Hea
 	}
 	return NewSQSHealthChecker(client, name, queueName)
 }
-func NewSQSHealthChecker(client *sqs.SQS, name string, queueName string) *HealthChecker {
-	return &HealthChecker{Client: client, QueueName: &queueName, Service: name}
+func NewSQSHealthChecker(client *sqs.SQS, name string, queueName string, options ...time.Duration) *HealthChecker {
+	var timeout time.Duration
+	if len(options) >= 1 && options[0] > 0 {
+		timeout = options[0]
+	} else {
+		timeout = 4 * time.Second
+	}
+	return &HealthChecker{Client: client, QueueName: &queueName, Service: name, Timeout: timeout}
 }
 
 func (h *HealthChecker) Name() string {
@@ -30,6 +38,7 @@ func (h *HealthChecker) Name() string {
 
 func (h *HealthChecker) Check(ctx context.Context) (map[string]interface{}, error) {
 	res := make(map[string]interface{})
+	h.Client.Config.HTTPClient.Timeout = h.Timeout
 	_, err := h.Client.GetQueueUrl(&sqs.GetQueueUrlInput{
 		QueueName: h.QueueName,
 	})
