495d351284603abdb82055d8db30301aa9992545
Duc Nguyen <minhduc140583@gmail.com>
2021-09-26 21:20:26 -0400

    qObject

--- ibm-mq/health_checker.go
+++ ibm-mq/health_checker.go
@@ -79,6 +79,7 @@ func (s *HealthChecker) Check(ctx context.Context) (map[string]interface{}, erro
 	if s.init == false {
 		var qObject ibmmq.MQObject
 		s.qObject = &qObject
+		s.init = true
 	}
 	subscriptionObject, err = s.queueManager.Sub(sd, s.qObject)
 
